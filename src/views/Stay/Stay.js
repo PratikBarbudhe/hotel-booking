1bbca6b0276fb850f6223748f59956ffa2a506f8
achalkatkar <11achalkatkar@gmail.com>
2023-10-08 22:06:06 +0530

    added

--- src/views/Stay/Stay.js
+++ src/views/Stay/Stay.js
@@ -4,15 +4,25 @@ import './Stay.css';
 
 function Stay(){
     return(
-        <div className="backgroung-image" >
+        <div>
+         <div className="backgroung-image" >
             <h1 className='heading-1'>-</h1>
             <h1 className='heading'>Howzat for a perfect stay</h1>
             <p className='text'>Search hotels, homes and much more</p>
             <button className='btn'>Discover More</button>
-            <input type='text' className='search' placeholder='Where are you going ?' />
-            <input type='text'  />
-            <input type='text'  placeholder='Where are you going ?' />
-            </div>
+
+          </div>  
+
+             <div>
+               <input type='checkbox' id='travel-1' className='checkbox' />
+               <label for='travel-1'>I'm travelling for work</label> &nbsp; &nbsp; &nbsp; 
+
+               <input type='checkbox' id='flight' />
+               <label for='flight'>I'm looking for flights</label>
+              </div>
+        </div>    
+
+           
 
     )
 }
