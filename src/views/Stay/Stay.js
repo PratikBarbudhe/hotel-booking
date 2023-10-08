924b48c60bcaf5540fade62a255843bd1c6130dc
achalkatkar <11achalkatkar@gmail.com>
2023-10-08 20:25:20 +0530

    added heading

---
+++ src/views/Stay/Stay.js
@@ -0,0 +1,16 @@
+import './Stay.css';
+
+
+
+function Stay(){
+    return(
+        <div className="backgroung-image" >
+            <h1 className='heading-1'>-</h1>
+            <h1 className='heading'>Howzat for a perfect stay</h1>
+            <p className='text'>Search hotels, homes and much more</p>
+            </div>
+
+    )
+}
+
+export default Stay
