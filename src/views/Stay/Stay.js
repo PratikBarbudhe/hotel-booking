95bcec6994b97dd6271083e9fc6c6ded2993bc7e
achalkatkar <11achalkatkar@gmail.com>
2023-10-08 20:33:06 +0530

    added

--- src/views/Stay/Stay.js
+++ src/views/Stay/Stay.js
@@ -8,6 +8,8 @@ function Stay(){
             <h1 className='heading-1'>-</h1>
             <h1 className='heading'>Howzat for a perfect stay</h1>
             <p className='text'>Search hotels, homes and much more</p>
+            <button className='btn'>Discover More</button>
+            
             </div>
 
     )
