713e2e34424d251283c71c09fdbaffba3cea84fb
achalkatkar <11achalkatkar@gmail.com>
2023-10-09 10:03:40 +0530

    added offers heading

--- src/views/Stay/Stay.js
+++ src/views/Stay/Stay.js
@@ -20,8 +20,20 @@ function Stay(){
                <input type='checkbox' id='flight' />
                <label for='flight' className='checkbox-label'>I'm looking for flights</label>
               </div>
+
               <h2 className='sub-heading'> Offers</h2>
-              <p>Promotions, deals and special offers for you</p>
+              <p className='sub-text'>Promotions, deals and special offers for you</p>
+               
+               <div className='sub-container'>
+                <div>
+                  <h2>Fly away to your dream holiday</h2>
+                </div>
+                <div>
+                  <h2>Take your longest holiday yet</h2>
+                </div>
+               </div>
+
+
               
         </div>    
         
