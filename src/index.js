8f2696409f950438f05d666d3ba048850ebf0d95
PratikBarbudhe <barbudhepratik03@gmail.com>
2023-10-10 14:20:01 +0530

    creating Flight And Hotel Page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Stay from './views/Stay/Stay';
+// import Flight from './views/Flight/Flight';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
