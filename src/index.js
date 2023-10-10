eb8e7edb5fddb5aa908b74efb4e50bc6e0eaea53
KaveriTupe25 <tupekaveri218@gmail.com>
2023-10-11 01:30:22 +0530

    background imaged added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Stay from './views/Stay/Stay';
-// import Flight from './views/Flight/Flight';
+import Flight from './views/Flight/Flight';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Stay/>
-);
+root.render( 
+    <Flight/>
+
+)
