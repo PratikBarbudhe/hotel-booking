32f9de3940c13b29bb35bc3746687402e0ef3919
achalkatkar <11achalkatkar@gmail.com>
2023-10-12 00:28:19 +0530

    added btn

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
+
+const MultiInputs = () => {
+
+    const [userReistration, setUserRegistration] = useState({
+        username: " ",
+        email: " ",
+        phone: " ",
+        password: " "
+    });
+
+   const handleInput = () => {
+
+   }
+
+}
 
 function Register () {
     return(
@@ -7,25 +22,38 @@ function Register () {
         <form action="" className="register-container">
             <div>
                 <lable for="username">username</lable>
-                <input type="text" name="usename" id="username"/>
+                <input type="text" autoComplete="off" 
+                 value={userReistration.username}
+                 onChange={handleInput}
+                name="usename" id="username"/>
+            
             </div>
 
             <div>
                 <lable for="email">email</lable>
-                <input type="email" name="email" id="email"/>
+                <input type="email" autoComplete="off" 
+                  value={userReistration.email}
+                  onChange={handleInput}
+                name="email" id="email"/>
             </div>
 
             <div>
                 <lable for="phone">phone</lable>
-                <input type="text" name="phone" id="phone"/>
+                <input type="text" autoComplete="off"
+                  value={userReistration.phone}
+                  onChange={handleInput}
+                name="phone" id="phone"/>
             </div>
 
             <div>
                 <lable for="password">password</lable>
-                <input type="text" name="password" id="password"/>
+                <input type="text" autoComplete="off" 
+                  value={userReistration.password}
+                  onChange={handleInput}
+                name="password" id="password"/>
             </div>
 
-            <button>Registration</button>
+            <button type="submit">Registration</button>
         </form>
 
         </>
