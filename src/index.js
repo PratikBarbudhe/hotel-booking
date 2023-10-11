59a0e127ee07f3b9faf0920474017f667103bb59
PratikBarbudhe <barbudhepratik03@gmail.com>
2023-10-11 19:56:54 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Stay from './views/Stay/Stay';
-
-
+<<<<<<< main
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
 );
+import Flight from './views/Flight/Flight';
+import Login from './views/Login/Login';
+import Register from "./views/Register/Register"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Stay/>,
+    },
+    {
+      path: "/flight",
+      element: <Flight/>,
+    },
+    {
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"Register",
+        element:<Register/>
+    }
+  ])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( 
+    <RouterProvider router={router} />
+
+)
+>>>>>>> main
