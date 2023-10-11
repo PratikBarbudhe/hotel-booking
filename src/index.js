e771f6591aae97a87331d6bd97b50802b8ce2b5a
suyash <suyashshinde76@gmail.com>
2023-10-11 22:40:58 +0530

    changes in carrental

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Stay from './views/Stay/Stay'; 
+// import Stay from './views/Stay/Stay'; 
 import CarRental from './views/Car-Rental/CarRental';
 
 const root = document.getElementById('root');
