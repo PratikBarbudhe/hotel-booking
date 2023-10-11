004a07d9492a2dfcb3b1d59409b237d07f7f6c03
UrmilaPawar1909 <urmilapawar221@gmail.com>
2023-10-11 19:25:02 +0530

    Add Attraction Page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Stay from './views/Stay/Stay';
+import Stay from './views/Stay/Stay'
+import Attractions from './views/Attractions/Attractions';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
