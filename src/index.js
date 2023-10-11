597cbe782ddbe47a8ff8b2ed773fe0de12ff99d6
suyash <suyashshinde76@gmail.com>
2023-10-11 17:43:18 +0530

    Car-Rental added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import Stay from './views/Stay/Stay'; 
-import CarRental from './views/Car-Rental/CarRental'
-=======
-import ReactDOM from 'react-dom/client';
-import Stay from './views/Stay/Stay';
-// import Flight from './views/Flight/Flight';
->>>>>>> 8c209d6989a10abdd04f048d1e03ebf625cb1167
+import CarRental from './views/Car-Rental/CarRental';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
   <div>
   
-
+    <Stay />
   </div>,
   root
 );
