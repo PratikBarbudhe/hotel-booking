d23c468888eb806e149f596c1e999ce7aa8ee3a0
achalkatkar <11achalkatkar@gmail.com>
2023-10-12 00:11:23 +0530

    added registration page

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -3,7 +3,28 @@ import React from "react";
 function Register () {
     return(
         <>
-        
+        <form action="">
+            <div>
+                <lable for="username">username</lable>
+                <input type="text" name="usename" id="username"/>
+            </div>
+
+            <div>
+                <lable for="email">email</lable>
+                <input type="email" name="email" id="email"/>
+            </div>
+
+            <div>
+                <lable for="phone">phone</lable>
+                <input type="text" name="phone" id="phone"/>
+            </div>
+
+            <div>
+                <lable for="password">password</lable>
+                <input type="text" name="password" id="password"/>
+            </div>
+        </form>
+
         </>
     )
 
