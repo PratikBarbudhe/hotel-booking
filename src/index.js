cdd6770ea18531cbf2982c3cce8a13bb09513514
PratikBarbudhe <barbudhepratik03@gmail.com>
2023-10-11 19:48:43 +0530

    conflict solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import Stay from './views/Stay/Stay';
-import Flight from './views/Flight/Flight';
+import Stay from './views/Stay/Stay';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Flight/>
+  
 );
