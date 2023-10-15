d1188fea117e29ef119e369f8f16af2a3c3f771c
suyash <suyashshinde76@gmail.com>
2023-10-15 09:20:07 +0530

    update

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
             <Link className="nav-menu" to="/Hotel">Hotel</Link>
             <Link className="nav-menu"  to="/flight">Flight</Link>
             <Link className="nav-menu"  to="/attraction">Attraction</Link>
-            <Link className="nav-menu"  to="/CarRental">Car Rental</Link>
+            <Link className="nav-menu"  to="/CarRental">CarRental</Link>
             <Link className="nav-menu"  to="/login">Login</Link>
             <Link className="nav-menu"  to="/register">Register</Link>
            
