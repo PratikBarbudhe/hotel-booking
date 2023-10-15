aa955775e9b7e9423d71974a6b825b37f0bb88ce
UrmilaPawar1909 <urmilapawar221@gmail.com>
2023-10-15 15:55:33 +0530

    Added more Cards

--- src/views/Attractions/Attractions.js
+++ src/views/Attractions/Attractions.js
@@ -7,29 +7,38 @@ import Agra from "./agra.jpg";
 import Oldgao from "./old gao.jpg";
 import London from "./London.webp";
 import Paris from "./paris.jpg";
+import Srilanka from "./srilanka.jpg";
+import Korea from "./korea.webp";
+import Singapore from "./singapur.jpg";
+import Explore from "./explore.png";
 
-function Attractions(){
-    return(
+function Attractions() {
+    return (
         <>
-        <div className="title-container">
-        <Navbar/>
-        
-        <p className="attractions-title"><b>Attractions, activities and <br/>experiences</b></p>
-        <p className="attractions-subtitle"> Discover new attractions and experiences to match your<br/> interests and travel style</p>
-        <div className="place-container">
-        <input type="text" placeholder="Where are you going" className="place-search"/>
-        
-        <input type="date" placeholder="Select yours date" className="place-date"/>
-        </div>
-        </div>
-        <h3 className="heading1">Nearby Destinations</h3>
-        <div>
-            <Cards img={kolkata} name="kolkata" info="15 things to do"   />
-            <Cards img={Agra} name="Agra" info="20 Things To Do "   />
-            <Cards img={Oldgao} name="Old Gao" info="30 things to do" />
-            <Cards img={London} name="London" info="25 things to do"/>
-            <Cards img={Paris} name="Paris" info="40 things to do"/>
-        </div>
+            <div className="title-container">
+                <Navbar />
+
+                <p className="attractions-title"><b>Attractions, activities and <br />experiences</b></p>
+                <p className="attractions-subtitle"> Discover new attractions and experiences to match your<br /> interests and travel style</p>
+                <div className="place-container">
+                    <input type="text" placeholder="Where are you going" className="place-search" />
+
+                    <input type="date" placeholder="Select yours date" className="place-date" />
+                </div>
+            </div>
+            <h1 className="heading1">Nearby Destinations🌍</h1>
+            <div className="main-card-container">
+                <Cards img={kolkata} name="kolkata" info="15 things to do" />
+                <Cards img={Agra} name="Agra" info="20 Things To Do " />
+                <Cards img={Oldgao} name="Old Gao" info="30 things to do" />
+                <Cards img={London} name="London" info="25 things to do" />
+                <Cards img={Paris} name="Paris" info="40 things to do" />
+                <Cards img={Srilanka} name="Srilanka" info="51 things to do " />
+                <Cards img={Korea} name="Korea" info="23 things to do" />
+                <Cards img={Singapore} name="Singapore" info="40 things to do" />
+            </div>
+
+            
         </>
     )
 
