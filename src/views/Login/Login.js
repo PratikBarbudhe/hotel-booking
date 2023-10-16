e44aa4b269754018d6e8b6f1493cc8db4115f385
shravanbhoyar1 <shravanbhoyar3507>
2023-10-16 07:00:57 +0530

    sign up page added

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.css";
+import Navbar from '../../component/Navbar/Navbar';
 
 function Login(){
-    return(
-        <div>
-            <h1>login page</h1>
+
+    const [action,setAction] = useState("Login");
+
+    return(<>
+        <Navbar/>
+        <div className="container">
+        <div className="header">
+         <div className="text">{action}</div>  
+         <div className="underline"></div> 
         </div>
+        <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+           
+            <input type="text" placeholder="Name"/>
+        </div> }
+         
+        <div className="input">
+            
+            <input type="email" placeholder="Email Id"/>
+        </div>
+        <div className="input">
+           
+            <input type="password" placeholder="Password"/>
+        </div>  
+        </div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+        
+        <div className="submit-container">
+            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
+        
+    
+        </div>
+        </>
+  
+        
+
     )
 }
 
