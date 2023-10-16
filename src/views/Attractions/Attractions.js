ba6fde19a728e7034a40b633ab57b88be48a3117
UrmilaPawar1909 <urmilapawar221@gmail.com>
2023-10-16 12:19:33 +0530

    update page

--- src/views/Attractions/Attractions.js
+++ src/views/Attractions/Attractions.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom/client';
 import "./Attractions.css"
 import kolkata from "./kolkata.jpg"
 import Navbar from "../../component/Navbar/Navbar";
@@ -10,7 +11,46 @@ import Paris from "./paris.jpg";
 import Srilanka from "./srilanka.jpg";
 import Korea from "./korea.webp";
 import Singapore from "./singapur.jpg";
-import Explore from "./explore.png";
+import Europe from "./Europe";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import AttractionNavbar from "../../component/AttractionNavabar/AttractionNavbar";
+
+const router = createBrowserRouter([
+    {
+      path: "/europe",
+      element: <Europe/>,
+    },
+   /* {
+        path: "/northamerica",
+        element: < NorthAmerica/> ,
+    },
+   {
+     path:"/asia",
+     element:<Asia/>
+   },
+    {
+        path: "/africa",
+        element: < Africa/> ,
+    },
+    {
+        path: "/southafrica",
+        element: <SouthAmerica />
+    },
+    {
+        path: "centralamerica",
+        element: <CentralAmerica/>
+    }*/
+    
+  ])
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( <>
+  < RouterProvider router = { router }
+    />
+   
+    </>
+);
 
 function Attractions() {
     return (
@@ -21,9 +61,9 @@ function Attractions() {
                 <p className="attractions-title"><b>Attractions, activities and <br />experiences</b></p>
                 <p className="attractions-subtitle"> Discover new attractions and experiences to match your<br /> interests and travel style</p>
                 <div className="place-container">
-                    <input type="text" placeholder="Where are you going" className="place-search" />
+                    <input type="text" placeholder="Where are you going" className="search" />
 
-                    <input type="date" placeholder="Select yours date" className="place-date" />
+                    <input type="date" placeholder="Select yours date" className="date" />
                 </div>
             </div>
             <h1 className="heading1">Nearby Destinations🌍</h1>
@@ -36,6 +76,16 @@ function Attractions() {
                 <Cards img={Srilanka} name="Srilanka" info="51 things to do " />
                 <Cards img={Korea} name="Korea" info="23 things to do" />
                 <Cards img={Singapore} name="Singapore" info="40 things to do" />
+
+                <hr className="hr"/>
+            </div>
+            <div className="destination-container">
+                <h2>Explore more destinations</h2>
+                <p>Find things to do in cities around the world</p>
+             <hr/>
+
+            <AttractionNavbar/>
+
             </div>
 
             
---
+++ src/views/Attractions/Europe.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Europe.css";
+import Navbar from "../../component/Navbar/Navbar"
+
+export default function Europe(){
+    <>
+
+    <Navbar/>
+    </>
+}
