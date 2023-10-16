d15d238b7f912622ed3b4438e4f4eaf98a31de4e
PratikBarbudhe <barbudhepratik03@gmail.com>
2023-10-16 10:17:16 +0530

    Update Changes

--- src/component/Hotelcard/Hotelcomp.js
+++ src/component/Hotelcard/Hotelcomp.js
@@ -1,43 +1,18 @@
 import React from 'react'
-// import "./Hotelcomp.css";
+ import "./Hotelcomp.css";
 
-const Card = ({ title, description, imageUrl }) => {
-  return (
-    <div style={styles.card}>
-      <img src={imageUrl} alt="card" style={styles.image} />
-      <div style={styles.cardBody}>
-        <h3 style={styles.title}>{title}</h3>
-        <p style={styles.description}>{description}</p>
+ const Card = ({ title, description, imageUrl }) => {
+    return (
+      <div className="card">
+        <img src={imageUrl} alt="card" className="image" />
+        <div className="cardBody">
+          <h3 className="title">{title}</h3>
+          <p className="description">{description}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-const styles = {
-  card: {
-    border: '1px solid #ddd',
-    borderRadius: '5px',
-    padding: '10px',
-    margin: '10px',
-    width: '300px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    transition: 'box-shadow 0.3s',
-  },
-  image: {
-    width: '100%',
-    borderRadius: '5px',
-  },
-  cardBody: {
-    padding: '10px',
-  },
-  title: {
-    fontSize: '1.5rem',
-    margin: '5px 0',
-  },
-  description: {
-    fontSize: '1rem',
-    color: '#555',
-  },
-};
+
 
 export default Card;
